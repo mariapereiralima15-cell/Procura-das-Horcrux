02e564b7ed975e28b3a3724d859e24fa0940553b
mariapereiralima15-cell <maria.pereira.lima15@escola.pr.gov.br>
2025-09-11 11:51:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const avanca = document.querySelectorAll('.btn-proximo')
+ const avanca = document.querySelectorAll('.btn-proximo')
 
 avanca.forEach(button => {
-    button.addEventListener('Click')
+    button.addEventListener('Click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.ClassList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
